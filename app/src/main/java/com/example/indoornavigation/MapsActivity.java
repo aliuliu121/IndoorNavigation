1950bf1bb6541e9779a8547044968728bcb4a43a
Allen Liu <allenliu225@gmail.com>
2019-09-25 14:23:27 -0400

    finished overlay code

--- app/src/main/java/com/example/indoornavigation/MapsActivity.java
+++ app/src/main/java/com/example/indoornavigation/MapsActivity.java
@@ -14,6 +14,7 @@ import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.UiSettings;
 import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.GroundOverlay;
@@ -38,6 +39,8 @@ public class MapsActivity extends FragmentActivity implements SeekBar.OnSeekBarC
 
     private CheckBox toggleOverlayLock;
 
+    private UiSettings mUiSettings;
+
     public void onMapSearch(View view) {
         EditText locationSearch = findViewById(R.id.locationText);
         String location = locationSearch.getText().toString();
@@ -57,8 +60,6 @@ public class MapsActivity extends FragmentActivity implements SeekBar.OnSeekBarC
             mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(latLng, 18.0f));
         }
     }
-
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -124,7 +125,17 @@ public class MapsActivity extends FragmentActivity implements SeekBar.OnSeekBarC
      */
     @Override
     public void onMapReady(GoogleMap googleMap) {
+        EditText locationSearch = findViewById(R.id.locationText);
+        locationSearch.bringToFront();
+
         mMap = googleMap;
+
+        mUiSettings = mMap.getUiSettings();
+
+        mUiSettings.setZoomControlsEnabled(false);
+        mUiSettings.setCompassEnabled(false);
+        mUiSettings.setMyLocationButtonEnabled(false);
+        mUiSettings.setTiltGesturesEnabled(false);
 
         CameraPosition cameraPosition = new CameraPosition.Builder()
                 .target(UC)      // Sets the center of the map to UC
